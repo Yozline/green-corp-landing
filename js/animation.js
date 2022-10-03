b1d871aa2f55f68bfe002a404fc589260be39788
Yozline <kr1sone@mail.ru>
2022-10-03 23:27:23 +0300

    добавление поля другое в выбор бюджета

--- js/animation.js
+++ js/animation.js
@@ -18,3 +18,24 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0,element,5000)
 }
 initIncreaseNumberAnimation();
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+        // Должны добавить еще одно текстовое поле
+        let formContainer = document.createElement("div");
+        formContainer.classList.add("form__group");
+        formContainer.classList.add("form__other-input");
+        let input = document.createElement("input");
+        input.placeholder = "Введите ваш вариант";
+        input.type = "text";
+        formContainer.appendChild(input);
+        document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+
+    let otherInput = document.querySelector(".form__other-input");
+    if (event.target.value !== 'other' && Boolean(otherInput)) {
+        // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+        let otherInput = document.querySelector(".form__other-input");
+        document.querySelector(".form form").removeChild(otherInput);
+    }
+})
