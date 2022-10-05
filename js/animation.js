599a31e7e88e3e43923b3b66823c2a2ce47524ec
Yozline <kr1sone@mail.ru>
2022-10-05 22:30:34 +0300

    добавление плавного скролла

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,5 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 30;
+let animationInited = false;
 function increaseNumberAnimationStep(i, element, endNumber) {
     if(i <= endNumber) {
         if (i === endNumber) {
@@ -17,7 +18,6 @@ function initIncreaseNumberAnimation() {
     let element = document.querySelector("div .features__clients-count");
     increaseNumberAnimationStep(0,element,5000)
 }
-initIncreaseNumberAnimation();
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
     if (event.target.value === 'other') {
@@ -39,3 +39,37 @@ document.querySelector('#budget').addEventListener('change', function handleSele
         document.querySelector(".form form").removeChild(otherInput);
     }
 })
+
+function updateScroll() {
+    if (window.scrollY > 0) {
+        let header = document.querySelector("header");
+        header.classList.add("header__scrolled")
+    } else {
+        let header = document.querySelector("header");
+        header.classList.remove("header__scrolled")
+    }
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if(windowBottomPosition > countElementPosition && !animationInited) { 
+        animationInited = true;
+        initIncreaseNumberAnimation();
+    }
+}
+window.addEventListener('scroll', updateScroll);
+
+
+function addSmoothScroll(anchor) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    addSmoothScroll(anchor);
+});
+
+addSmoothScroll(document.querySelector(".more-button"));
